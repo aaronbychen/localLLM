b2ddc80d6696d18a5203e4f08d8c181559a53f4e
aaronchen <aaronbychen@gmail.com>
2024-11-18 16:16:50 -0600

    updated token

--- runningVersion.py
+++ runningVersion.py
@@ -29,7 +29,7 @@ client = AsyncOpenAI(api_key=API_KEY, base_url=BASE_URL)
 settings = {
     "model": "llama-3.1-405b",
     "temperature": 0,
-    "max_tokens": 8192,
+    "max_tokens": 4096,
     "top_p": 1,
     "frequency_penalty": 0,
     "presence_penalty": 0,
